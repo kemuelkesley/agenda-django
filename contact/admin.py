2423e8532c741c9bc1a003bc9d27d4ac042c2104
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-11-03 13:28:25 -0300

    nova arquitetura de views adicionada.

--- contact/admin.py
+++ contact/admin.py
@@ -5,21 +5,24 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone', 'email', 'created_date', 'description')
+    list_display = ('id' ,'first_name', 'last_name', 'phone', 'email', 'created_date', 'description')
     #list_filter = ('created_date',)
     search_fields = ('first_name', 'last_name', 'phone', 'email', 'description')
     ordering = ('created_date',)
     date_hierarchy = 'created_date'
     #list_editable = ('phone', 'email',)
-    fieldsets = (
-        ('Informações', {
-            'fields': ('first_name', 'last_name', 'phone', 'email')
-        }),
-        ('Descrição', {
-            'fields': ('description',)
-        }),
-    )
+    # fieldsets = (
+    #     ('Informações', {
+    #         'fields': ('first_name', 'last_name', 'phone', 'email')
+    #     }),
+    #     ('Descrição', {
+    #         'fields': ('description',)
+    #     }),
+    # )
     
     
 
-
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Categorie'
+
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -10,7 +22,24 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(max_length=200,blank=True)
-   
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, 
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        blank=True, 
+        null=True
+    )
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+    
+
+    class Meta:
+        verbose_name = 'Contacto'
+        
