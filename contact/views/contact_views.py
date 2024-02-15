aef8c8a6eab7f84589749f959b00c12583c4a06e
kemuelkesley <kemuelkesleytkd@outlook.com>
2024-02-15 20:52:57 -0300

    Criada o view para buscar por Id

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404
+
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
 
 
 def index(request):
@@ -8,8 +11,30 @@ def index(request):
         .filter(show=True)\
         .order_by('-id')
 
+    print(contacts.query)
+
     context = {
         'contacts': contacts,
     }
 
     return render(request, 'contact/index.html', context)
+
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+
+    if single_contact is None:
+        raise Http404('Contact not found')
+
+    context = {
+        'contact': single_contact,
+    }
+
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
